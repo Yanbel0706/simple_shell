781df2f0bd8fddf0acf0d763386a5788499ad7be
yanbel0706 <yanis.belk.edu@gmail.com>
2023-11-15 20:14:24 +0100

    betty check for my files

--- path_operation.c
+++ path_operation.c
@@ -1,35 +1,35 @@
 #include "shell.h"
 #include <stdlib.h>
 /**
- ** parse_path - Analyse la variable d'environnement PATH en listant les répertoires.
+** parse_path - Analyse la variable d'environnement
+** PATH en listant les répertoires.
  ** Return: Un tableau de répertoires extraits de la variable PATH.
  **/
 char **parse_path(void)
 {
-     int cpt = 1, n = 0;
-     char *path, **dirs, *token = NULL;
-     
-     path = getenv("PATH");
-     if (path == NULL)
-	  return (NULL);
-    
-     while (path[n] != '\0')
-     {
-        if (path[n] == ':')
-	   cpt++;
-	n++;
-     }
-     dirs = malloc(sizeof(char *) * (cpt + 1));
-     n = 0;
-     token = strtok(path, ":");
-     while (token != NULL)
-     {
-        dirs[n] = my_strdup(token);
-	token = strtok(NULL, ":");
-	n++;
-     }
-     dirs[n] = NULL;
-     return (dirs);
+	int cpt = 1, n = 0;
+	char *path, **dirs, *token = NULL;
+
+	path = getenv("PATH");
+	if (path == NULL)
+		return (NULL);
+	while (path[n] != '\0')
+	{
+		if (path[n] == ':')
+			cpt++;
+		n++;
+	}
+	dirs = malloc(sizeof(char *) * (cpt + 1));
+	n = 0;
+	token = strtok(path, ":");
+	while (token != NULL)
+	{
+		dirs[n] = my_strdup(token);
+		token = strtok(NULL, ":");
+		n++;
+	}
+	dirs[n] = NULL;
+	return (dirs);
 }
 
 /**
@@ -40,33 +40,28 @@ char **parse_path(void)
  **/
 char *resolve_path(char *cmd, char **dirs)
 {
-    char *f_path;
-    int n = 0;
-    size_t cmdsize = strlen(cmd), arrsize;
-    
+	char *f_path;
+	int n = 0;
+	size_t cmdsize = strlen(cmd), arrsize;
 
-    if (access(cmd, X_OK) == 0)
-    {
-        return (cmd);
-    }
-    while (dirs[n] != NULL)
-    {
-        arrsize = strlen(dirs[n]);
-	f_path = malloc(sizeof(char) * (cmdsize + arrsize + 2));
-	strcpy(f_path, dirs[n]);
-	strcat(f_path, "/");
-	strcat(f_path, cmd);
-
-	if (access(f_path, X_OK) == 0)
+	if (access(cmd, X_OK) == 0)
 	{
-		free(f_path);
-		return f_path;
-		
+		return (cmd);
 	}
-	
-	free(f_path);
-	n++;
-    }
-    
-    return NULL;
+	while (dirs[n] != NULL)
+	{
+		arrsize = strlen(dirs[n]);
+		f_path = malloc(sizeof(char) * (cmdsize + arrsize + 2));
+		strcpy(f_path, dirs[n]);
+		strcat(f_path, "/");
+		strcat(f_path, cmd);
+		if (access(f_path, X_OK) == 0)
+		{
+		free(f_path);
+		return (f_path);
+		}
+		free(f_path);
+		n++;
+	}
+	return (NULL);
 }
--- replace_arg.c
+++ replace_arg.c
@@ -1,24 +1,23 @@
 #include "shell.h"
-
 /**
- ** rep_st_arg - Remplace le premier élément du tableau d'arguments par le chemin complet de
- **la commande.
+ ** rep_st_arg - Remplace le premier élément du tableau
+ ** d'arguments par le chemin complet de la commande.
  ** @args: Tableau de la commande et de ses arguments.
  ** @f_path: Chaîne représentant le chemin complet de la commande.
  ** Return: Tableau de la commande avec le chemin complet.
- **/ 
+ **/
 
 char **rep_st_arg(char **args, const char *f_path)
 {
-    if (args == NULL || f_path == NULL) 
-        {
-	return args;
+	if (args == NULL || f_path == NULL)
+	{
+	return (args);
 	}
-    free(args[0]);
-    args[0] = my_strdup(f_path);
-    if (args[0] == NULL) 
-    {
-	    return args;
-    }
-    return args;
+	free(args[0]);
+	args[0] = my_strdup(f_path);
+	if (args[0] == NULL)
+	{
+		return (args);
+	}
+	return (args);
 }
